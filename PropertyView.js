2253e56de0963ca317fda25ee23059a89a310426
Danny Gilbert <danny_gilbert@icloud.com>
2017-10-16 17:00:19 -0500

    Completed Challenge to display property on selection

--- PropertyView.js
+++ PropertyView.js
@@ -9,27 +9,30 @@ import {
 } from 'react-native';
 
 export default class PropertyView extends Component {
-
     render() {
         var property = this.props.property;
-        var stats = property.bedroom_number + ' bed ' + (property.bathroom_number > 1 ? 'bathrooms' : 'bathroom');
-    }
+        var stats = property.bedroom_number + ' bed ' + property.property_type;
+        if (property.bathroom_number) {
+            stats += ', ' + property.bathroom_number + ' ' + (property.bathroom_number > 1
+                ? 'bathrooms' : 'bathroom');
+        }
 
-    var price = property.price_formatted.split(' ')[0];
+        var price = property.price_formatted.split(' ')[0];
 
-    return (
-        <View style={styles.container}>
-            <Image style={styles.image}
-                source={{uri: property.img_url}} />
-            <View style={styles.heading}>
-                <Text style={styles.price}>{price}</Text>
-                <Text style={styles.title}>{property.title}</Text>
-                <View style={styles.separator}/>
+        return (
+            <View style={styles.container}>
+                <Image style={styles.image}
+                       source={{uri: property.img_url}} />
+                <View style={styles.heading}>
+                    <Text style={styles.price}>{price}</Text>
+                    <Text style={styles.title}>{property.title}</Text>
+                    <View style={styles.separator}/>
+                </View>
+                <Text style={styles.description}>{stats}</Text>
+                <Text style={styles.description}>{property.summary}</Text>
             </View>
-            <Text style={styles.description}>{stats}</Text>
-            <Text style={styles.description}>{property.summary}</Text>
-        </View>
-    );
+        );
+    }
 }
 
 const styles = StyleSheet.create({
